2897ffb16f1b09d4831e0c358471c9132c9751ae
Polizzi12 <peppepolizzi73@gmail.com>
2024-01-22 17:23:24 +0100

    ultima modifica

---
+++ D1-1.js
@@ -0,0 +1,7 @@
+ let farina = 200;
+let zucchero = 150;
+let uova = 4;
+let latte = 250;
+let burro = 100;
+let somma=farina+zucchero+uova+latte+burro
+console.log("la somma del quantitativo degli ingredienti è:",somma)
